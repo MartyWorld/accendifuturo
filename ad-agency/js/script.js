fa18c835999b53c5a993dce53f165c94bebcb972
bilalmisla <bilal@madeinsouth.la>
2025-05-12 17:05:50 +0500

    added pages

--- ad-agency/js/script.js
+++ ad-agency/js/script.js
@@ -17,34 +17,16 @@ jQuery(function ($) {
        Nav Scroll
        ====================================== */
 
-    $(".scroll").on("click", function(event){
-        event.preventDefault();
-        $('html,body').animate({
-            scrollTop: $(this.hash).offset().top - 40}, 1100);
-    });
+    // $(".scroll").on("click", function(event){
+    //     event.preventDefault();
+    //     $('html,body').animate({
+    //         scrollTop: $(this.hash).offset().top - 40}, 1100);
+    // });
     /* ====================================
        Nav Fixed On Scroll
        ======================================= */
 
-    $(window).on('scroll', function () {
-        if ($(this).scrollTop() >= 80) { // Set position from top to add class
-            $('header').addClass('header-appear');
-        }
-        else {
-            $('header').removeClass('header-appear');
-        }
-    });
-    /*bottom menu fix*/
-    if ($("nav.navbar").hasClass("bottom-nav")) {
-        var navHeight = $(".bottom-nav").offset().top;
-        $(window).on("scroll", function () {
-            if ($window.scrollTop() > navHeight) {
-                $('header').addClass('header-appear');
-            } else {
-                $('header').removeClass('header-appear');
-            }
-        });
-    }
+
 
     /* ===================================
        Side Menu
