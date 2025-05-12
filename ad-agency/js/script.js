0067e8783fd22e9f11bcca1b7d3b18f51342be90
bilalmisla <bilal@madeinsouth.la>
2025-05-12 21:44:15 +0500

    update

--- ad-agency/js/script.js
+++ ad-agency/js/script.js
@@ -481,4 +481,4 @@ jQuery(function ($) {
     });
 });
 
-window.gtranslateSettings = { "default_language": "it", "languages": ["it", "en"], "wrapper_selector": ".gtranslate_wrapper", "flag_size": 24, "switcher_horizontal_position": "right", "switcher_text_color": "#f7f7f7", "switcher_arrow_color": "#f2f2f2", "switcher_border_color": "#161616", "switcher_background_color": "#303030", "switcher_background_shadow_color": "#474747", "switcher_background_hover_color": "#3a3a3a", "dropdown_text_color": "#eaeaea", "dropdown_hover_color": "#748393", "dropdown_background_color": "#474747" }
+window.gtranslateSettings = { "default_language": "it", "native_language_names": true, "detect_browser_language": true, "languages": ["it", "en"], "wrapper_selector": ".gtranslate_wrapper", "switcher_horizontal_position": "right", "switcher_text_color": "#f7f7f7", "switcher_arrow_color": "#f2f2f2", "switcher_border_color": "#161616", "switcher_background_color": "#303030", "switcher_background_shadow_color": "#474747", "switcher_background_hover_color": "#3a3a3a", "dropdown_text_color": "#eaeaea", "dropdown_hover_color": "#748393", "dropdown_background_color": "#474747" }
